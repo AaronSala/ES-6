440937eeb1621a7f79e00cff65a359360bdad961
Aaron Sala <salaaron2@gmail.com>
2023-11-01 12:39:12 +0300

    arrow function

--- es6/es6.js
+++ es6/es6.js
@@ -1,18 +1,10 @@
-"use strict";
-const person = {
-    name:"aaron",
-    walk(){
-        
-        console.log(this)
-    }
-    
-}
-console.log(this)
-person.walk()
-//console.lo
-function myFunction() {
-    x = 10; // This will throw an error in strict mode because 'x' is not declared.
-    console.log(x)  
-}
-  
-  myFunction();
+ "use strict"
+ let jobs =[
+    {id:1, isactive:true},
+    {id:2, isactive:true},
+    {id:3, isactive:false}
+ ]
+ 
+
+ const active = jobs.filter(job=>job.isactive)
+console.log(active)
