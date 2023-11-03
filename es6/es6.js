4e22e84f923397c95257b6a700f6a8b99691af3c
Aaron Sala <salaaron2@gmail.com>
2023-11-03 12:25:00 +0300

    included error messages

--- es6/es6.js
+++ es6/es6.js
@@ -1,21 +1,51 @@
-const person = {
-    name:"Aaron",
-    age:30,
-    school:"kitise",
-    sex:"male",
-    location:"kyambeke"
-}
-const person2 = {
-    name:"Sala",
+
+const form = document.forms.myform
+// const forms=document.getElementById('myform');
+// const name=document.getElementById('name');
+// const password=document.getElementById('password');
+// const error=document.getElementById('error');
+const nameInput = form.name;
+const passInput = form.password
+
+nameInput.addEventListener('input', () => {
+    if (nameInput.value.length > 4) {
+      nameInput.style.border = "1px solid green";
+      error.style.display="none"
+    } else {
+      nameInput.style.border = "1px solid red";
+    }
+  });
+
+  passInput.addEventListener('input', () => {
+    if (passInput.value.length === 8) {
+      passInput.style.border = "1px solid green";
+      passerror.style.display="none"
+    } else {
+      passInput.style.border = "1px solid red";
+    }
+  });
+
+form.addEventListener('submit',(e)=>{
+    let messages = []
+    let passmessages = []
+    if(nameInput.value==="" || nameInput.value ===null){
+        messages.push("name is required")
+        error.innerHTML=messages.join(", ");
+        nameInput.style.border="1px solid red"
+        e.preventDefault()
+
+    }
+    else{
+        error.style.display="none"
+        nameInput.style.border="1px solid green"
+    }
     
-    sex:"male",
-    location:"ndolo"
-}
-
-const person3= {...person, ...person2} 
-
-console.log(person3);
-
-
-
-
+    if(passInput.value.length<8 || passInput.value ===""){
+       
+        passmessages.push("password must be upto 8 characters")
+        passInput.style.border="1px solid red"
+        passerror.innerHTML=passmessages.join(", ");
+        e.preventDefault()
+    }
+    
+})
