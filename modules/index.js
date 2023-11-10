97969f64a51c18e6d93f00645eec4ff165aafc6e
Aaron Sala <salaaron2@gmail.com>
2023-11-10 16:26:01 +0300

    sorting

--- modules/index.js
+++ modules/index.js
@@ -1,7 +1,23 @@
-import sum from'./login.js';
+const array =[ 34, 29]
 
 
-//console.log(sub(9, 4));
-console.log(sum(9, 4));
 
 
+function sorted(arr){
+const uniquieArr = Array.from(new Set(arr))
+console.log(uniquieArr);
+ uniquieArr.sort((a,b)=>{
+    return b-a
+    
+})
+if(uniquieArr.length>=2){
+    return uniquieArr[1];
+}
+else{
+    return -1
+}
+console.log(uniquieArr)
+
+
+}
+console.log(sorted(array)) 
